fc74a36895c91a9a192d9e80281c96b6c4641fb0
michael.cizmar@mcplusa.com <michael.cizmar@mcplusa.com@dcd2e894-6ba1-bb33-2d5f-46855b01ad59>
2011-10-17 02:36:43 +0000

    copying over bug fixes to current issues

--- trunk/pika/home/pika/createNoteFromGoogleDoc.php
+++ trunk/pika/home/pika/createNoteFromGoogleDoc.php
@@ -16,15 +16,72 @@ if(isset($_GET['token']) && $_GET['token']) {
 require_once ('pika-danio.php');
 pika_init();
 require_once('pikaGoogleSync.php');
+require_once('pikaUser.php');
 require_once('Zend/Loader.php');
 Zend_Loader::loadClass('Zend_Debug');
 
 $obj = new pikaGoogleSync();
 $obj->connect();
 
+$saveAttachments = $_REQUEST['save_attachments'];
+if($saveAttachments == '1')
+{     
+  $user = new pikaUser($auth_row['user_id']);
+  $email = $user->getValue('email');
+  
+  $obj->conectImapProtocol($email);
+  
+  //Get the msg id usin the X-GM-MSGID
+  $obj->_protocol->examine();
+  $xg_gm_msidg = $obj->hex2dec($_REQUEST['xgmmsgid']);  
+  $response = $obj->_protocol->requestAndResponse("UID SEARCH X-GM-MSGID " . $xg_gm_msidg);
+  if(is_null($response) || $response==false || !isset($response[0][1]))
+  {
+  	echo "Could not find the message<br/>";
+  	return;
+  }  	    
+  $msgid = $response[0][1];      
+  
+  //Get the message with the obtained id
+  $number = $obj->_storage->getNumberByUniqueId($msgid);
+  $msg = $obj->_storage->getMessage($number);     
+  
+  $attachments = array(); 
+  
+  //Iterate message parts and get attachments
+  foreach (new RecursiveIteratorIterator($msg) as $part) {
+    //If part contains content-disposition header then it's an attachment part  
+    if($part->headerExists("content-disposition") && strtok($part->contentDisposition, ';') == 'attachment')
+    {      	 
+      //Get the attacment file name
+      $fileName = strtok('=');
+      $fileName = strtok('"');
+      str_replace("\"", "", $fileName);
+      $fileType = strtok($part->contentType, ';');
+      $fileContent = base64_decode($part->getContent());
+      $fileSize = strlen($fileContent);                	    	 
+      //Get the attachement and decode
+      $attachments[] = array("file_name" => $fileName, "file_type" => $fileType, "file_content" => $fileContent, "file_size" => $fileSize);	                    	
+    }
+  }
+  
+  require_once ('pika-danio.php');
+  require_once('./app/lib/pikaDocument.php');
+  
+  pika_init();  
+  
+  foreach ($attachments as $key => $value) {    
+    $doc = new pikaDocument();           
+    $doc->uploadDocFromContent($value['file_content'], $value['file_name'], $value['file_type'], $value['file_size'], "", 0, "C", $_REQUEST['case_id']);    
+  }
+}
+
 $document = $obj->getDoc($_REQUEST['document_id']);
 
 $notes = $obj->getDocumentBody($document->getContent()->getSrc());
+$notesWithNoDoubleLineBreaks = str_replace(chr(13).chr(10).chr(13).chr(10), chr(13).chr(10), $notes);
+
+
 
 require_once ('pika_cms.php');
 
@@ -34,12 +91,11 @@ $noteTime = $date['hours'] . ":" . $date['minutes'] . ":" . $date['seconds'];
 
 $_REQUEST['act_type'] = 'N';
 $_REQUEST['category'] = 'CS';  
-$_REQUEST['completed'] = '1';
-$_REQUEST['summary'] = 'Note Uploaded From Mail';  
+$_REQUEST['completed'] = '1'; 
 $_REQUEST['hours'] = '0';  
 $_REQUEST['act_date'] = $noteDate;
 $_REQUEST['act_time'] = $noteTime;
-$_REQUEST['notes'] = $notes;  
+$_REQUEST['notes'] = $notesWithNoDoubleLineBreaks;  
 
 // VARIABLES
 $pk = new pikaCms;
@@ -56,8 +112,8 @@ $act_id = $pk->newActivity($a);
 //$obj->deleteDoc($document);
 $document->delete();
 
-$alertMsg = 'Pika note successfully created';
-$alertMsg .=$_REQUEST['save_attachments']?" and attachments uploaded to case.":".";
+$alertMsg = 'Pika case note created';
+$alertMsg .=($saveAttachments == '1')?" and attachments uploaded to case.":".";
   
 $returnHtml ='
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>      	           
--- trunk/pika/home/pika/createTemporaryGoogleDoc.php
+++ trunk/pika/home/pika/createTemporaryGoogleDoc.php
@@ -40,54 +40,37 @@ foreach($_REQUEST as $key => $value)
 
 
 $obj = new pikaGoogleSync();
+
 $obj->connect();
+
 
 $user = new pikaUser($auth_row['user_id']);
 $email = $user->getValue('email');
 
 $obj->conectImapProtocol($email);
 
+
 //Get the msg id usin the X-GM-MSGID
-//$imapOauth->_protocol->examine();
+
 $obj->_protocol->examine();
-$xg_gm_msidg = /*$imapOauth*/$obj->hex2dec($_REQUEST['xgmmsgid']);  
-$response = /*$imapOauth*/$obj->_protocol->requestAndResponse("UID SEARCH X-GM-MSGID " . $xg_gm_msidg);
+$xg_gm_msidg = $obj->hex2dec($_REQUEST['xgmmsgid']);  
+$response = $obj->_protocol->requestAndResponse("UID SEARCH X-GM-MSGID " . $xg_gm_msidg);
 if(is_null($response) || $response==false || !isset($response[0][1]))
 {
-	echo 'no encontré el mensaje<br/>';
+	echo 'Could not fin the message<br/>';
 	return;
 }  	    
 $msgid = $response[0][1];      
 
 //Get the message with the obtained id
-$number = /*$imapOauth*/$obj->_storage->getNumberByUniqueId($msgid);
-$msg = /*$imapOauth*/$obj->_storage->getMessage($number);     
+$number = $obj->_storage->getNumberByUniqueId($msgid);
+$msg = $obj->_storage->getMessage($number);     
 
-$saveAttachments = $_REQUEST['save_attachments'];
-if($saveAttachments)
-{
-	$attachments = array(); 
-}
+//Iterate message parts and get text/plain and/or text/html
 
-//Iterate message parts and get attachments, text/plain and/or text/html
+$summary = $msg->subject;
 foreach (new RecursiveIteratorIterator($msg) as $part) {
-  //If part contains content-disposition header then it's an attachment part  
-  if($part->headerExists("content-disposition") && strtok($part->contentDisposition, ';') == 'attachment')
-  {
-    if($saveAttachments)
-    {	  
-      //Get the attacment file name
-      $fileName = strtok('=');
-      $fileName = strtok('"');
-      str_replace("\"", "", $fileName);
-      $fileType = strtok($part->contentType, ';');
-      $fileContent = base64_decode($part->getContent());
-      $fileSize = strlen($fileContent);                	    	 
-      //Get the attachement and decode
-      $attachments[] = array("file_name" => $fileName, "file_type" => $fileType, "file_content" => $fileContent, "file_size" => $fileSize);	                
-    }      	
-  }
-  else 
+  if(!$part->headerExists("content-disposition"))
   {
     if(strtok($part->contentType, ';') == 'text/plain'){
       $plain = $part->getContent();      
@@ -96,36 +79,20 @@ foreach (new RecursiveIteratorIterator($msg) as $part) {
       $html = $part->getContent();      	       
     }      	    
   }
-}       
+}
+
           
 //$_REQUEST['doc_data'] = isset($html)? $html : $plain;
 $_REQUEST['doc_data'] = isset($plain)? $plain : $html;
-  
 $_REQUEST['doc_name'] = "test.txt";
 $_REQUEST['folder_name'] = "temp_docs";
 $_REQUEST['convert'] = 1;
 $_REQUEST['mime_type'] = "text/plain";
 
-if($saveAttachments){
-  require_once ('pika-danio.php');
-  require_once('./app/lib/pikaDocument.php');
-  
-  pika_init();  
-  
-  foreach ($attachments as $key => $value) {    
-    $doc = new pikaDocument();           
-    $doc->uploadDocFromContent($value['file_content'], $value['file_name'], $value['file_type'], $value['file_size'], "", 0, "C", $_REQUEST['case_id']);    
-    //Save the attachment
-    /*$fh = fopen("C:/".$key.'.txt', 'w');
-    fwrite($fh, $value);    
-    fclose($fh);*/
-  }
-}
 
 $googleDoc = $obj->uploadDoc($_REQUEST);
 
-$documentId = str_replace("document%3A","",$googleDoc['document_uri']);
-
+$documentId = str_replace("https://docs.google.com/feeds/id/document%3A","",$googleDoc['document_uri']);
 //change to your apps domain
 $editDocumentUrl = 'https://docs.google.com/a/mcplusa-dev.com/document/d/'.$documentId.'/edit?hl=en_US';
 
@@ -133,30 +100,38 @@ $html ='<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs
       	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.min.js"></script>            
       	<script>
         	$(document).ready(function () {        		
-				$("#attachToCase").click(function(){					
-            var url = "/pika/createNoteFromGoogleDoc.php?case_id='.$_REQUEST['case_id'].'&document_id='.$documentId.'&save_attachments='.$_REQUEST['save_attachments'].'";		                            
-            window.open(url);
-            window.close();
-            /*
-            $.getJSON(url, function(data){
-              alert(data);	            		
-            })
-            .error(
-              function(jqXHR, textStatus, errorThrown) {
-                alert(textStatus); 
-            });
-            */
-          });																				                                             	
-        });                                              
-	  	</script> 
-       	<input type="button" id="attachToCase" value="Attach to Case"/>
-	   	<input type="hidden" id="caseId" value="'.$_REQUEST['case_id'].'"/>
-	   	<iframe src="' . $editDocumentUrl . '" width="860px" height="600px">';
-	   
-	   
-echo $html;	   
-	   
+				$("#attachToCase").click(function(){
+					var save_attachments = getSaveAttachments();					
+            		var url = "/pika/createNoteFromGoogleDoc.php?case_id='.$_REQUEST['case_id'].'&document_id='.$documentId.'&save_attachments=" + save_attachments + "&xgmmsgid='.$_REQUEST['xgmmsgid'].'&summary='.$summary.'";		                            
+            		window.open(url);
+            		window.close();            
+      			});
+      		});
 
-
-
+			function deleteTemporaryGoogleDoc()
+			{
+    		  var url = "/pika/deleteGoogleDoc.php?document_id='.$documentId.'";		                            
+              window.open(url);
+              window.close();
+			}
+			
+			function getSaveAttachments()
+            {
+              var save_attachments = "0";
+              if($("#saveAttachments").is(":checked")){
+                save_attachments = "1";
+              }
+              return save_attachments;
+            }
+	  	</script>	  	
+	  	<iframe src="' . $editDocumentUrl . '" width="860px" height="600px">
+	  	</iframe>
+	  	</br></br>
+       	<input type="button" style="width: 160px; font-weight: bold; font-size: 16px;" id="attachToCase" value="Attach to Case"/> 
+       	<a onClick="deleteTemporaryGoogleDoc(); return false;" href="#">Cancel</a>       	
+		<label for="saveAttachments" style="float:right;">Save Attachments to Case</label>
+		<input style="float:right;" type="checkbox" id="saveAttachments"';        
+$html.=($_REQUEST['save_attachments'])?'checked="checked">':'>';
+	   	   
+        echo $html;	   	  
 ?>
